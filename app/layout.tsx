d849e7ddf1a7161066e1c75d93a9e048c683c086
adammeg <adam.bhedj13@gmail.com>
2025-12-19 00:52:09 +0100

    fix version2

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,6 +104,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Preconnect and Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;800&family=Manrope:wght@400;600;700;800&display=swap" rel="stylesheet" />
         <meta name="google-site-verification" content="BFT7sOLB_7cvXTwCc5hPbi29Mtk-zdscHoY0bRwv2oA" />
         {/* Organization Schema */}
         <script
